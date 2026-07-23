cc7603f01586a1669aae1304031a8c77014f310e
harshithaendla <harshithaendla@gmail.com>
2026-07-23 12:49:34 +0530

    project

--- cls assignments/script.js
+++ cls assignments/script.js
@@ -1,2 +1,13 @@
-var brand = "samsung"
-console.log(brand)
+console.log("Javascript is connected")
+const year=new Date().getFullYear();
+document.querySelector('.site-footer p').innerHTML=`&copy;${year} Harshitha.All rights reserved.`;
+function getGreeting(){
+    const hour=new Date().getHours();
+    if(hour <12) return "Good Morning";
+    if(hour <17) return "Good Afternoon";
+    return "Good Evening";
+}
+const heroTitle =document.querySelector('.hero-section h1');
+if (heroTitle){
+    heroTitle.textContent = `${getGreeting()},I'm Harshitha`
+}
